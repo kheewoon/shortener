83bb4b3ddf7b2e973ab1614665b924dc7c4a4f2d
gmldns <gmldns1346@gmail.com>
2022-09-24 11:39:02 +0900

    프로젝트 생성

---
+++ src/main/java/org/report/shortener/ShortenerApplication.java
@@ -0,0 +1,13 @@
+package org.report.shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortenerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/report/shortener/ShortenerService.java
@@ -0,0 +1,13 @@
+package org.report.shortener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ShortenerService {
+
+    @Test
+    void url단축(){
+
+    }
+}
